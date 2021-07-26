f76d1d041722a4246500ae287d0f19ab4c366ea0
Juan Ignacio Lopez <juan.lopez@indevelup.com>
2021-07-26 09:40:39 -0300

    cambios validacion aligment

--- src/actions/FormularioUsuarioActions.js
+++ src/actions/FormularioUsuarioActions.js
@@ -51,7 +51,7 @@ export const buscarHeroeApi = (heroe, setFieldValue, superHeroes) => async dispa
             type: HEROE_NOT_FOUND,
             payload: "El superheroe ya existe por favor ingrese otro"
         })
-    if (data.response === "success" && validarAlignment(superHeroes, data) === 3) {
+    if (data.response === "success" && validarAlignment(superHeroes, data) >= 3) {
         capacidad = true;
         dispatch({
             type: HEROE_NOT_FOUND,
@@ -63,10 +63,9 @@ export const buscarHeroeApi = (heroe, setFieldValue, superHeroes) => async dispa
         setFieldValue('id', data.results[0].id);
         setFieldValue('alineacion', data.results[0].biography.alignment);
         setFieldValue('imagen', data.results[0].image.url);
-        setFieldValue('powers', data.results[0].powerstats)
-        setFieldValue('weight', data.results[0].appearance.weight)
-        setFieldValue('height', data.results[0].appearance.height)
-
+        setFieldValue('powers', data.results[0].powerstats);
+        setFieldValue('weight', data.results[0].appearance.weight);
+        setFieldValue('height', data.results[0].appearance.height);
         dispatch({
             type: SEARCH_HEROE,
         });
@@ -80,12 +79,12 @@ const validarSuper = (superheroes, data) => {
         return o.id == data.results[0].id
     })
 
-    return (x<=0) ?false  : true;
+    return (x <= 0) ? false : true;
 }
 
 const validarAlignment = (superheroes, data) => {
     let x = _.sumBy(superheroes, function (o) {
-        return  (o["aligment"] === data.results[0].biography.alignment) ? 1 : 0
+        return (o["aligment"] === data.results[0].biography.alignment) ? 1 : 0
     });
     return x;
 }
